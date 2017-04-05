6241b15b43cbc928b8dcbb903f54d45b456b67ea
Jan Reuteler <janreuteler@icloud.com>
2017-04-05 11:40:48 -0500

    created custom CMSFields

--- dataobjects/Building.php
+++ dataobjects/Building.php
@@ -37,15 +37,27 @@ class Building extends DataObject
 
     public function getCMSFields()
     {
-        $fields = parent::getCMSFields();
+        $fields = FieldList::create(TabSet::create('Root'));
 
-
-        $conf=GridFieldConfig_RelationEditor::create(10);
-        $conf->addComponent(new GridFieldSortableRows('SortOrder'));
-
-        $fields->removeByName('Floors');
-
+        $fields->addFieldToTab('Root.Main', TextField::create('Title'));
+        $fields->addFieldToTab('Root.Main', TextareaField::create('Text'));
+        $conf = GridFieldConfig_RelationEditor::create();
         $fields->addFieldToTab('Root.Main', new GridField('Floors', 'Floors', $this->Floors(), $conf));
+
+        $conf = GridFieldConfig_RelationEditor::create(10);
+        $conf->addComponent(new GridFieldSortableRows('SortOrder'));
+        $fields->addFieldToTab('Root.Main', new GridField('Floors', 'Floors', $this->Floors(), $conf));
+
+        // Image / Offsets
+        $fields->addFieldToTab('Root.Image/Offsets', UploadField::create('RoofImage')->setAllowedExtensions('png', 'jpg', 'jpeg'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('BuildingOffsetX'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('BuildingOffsetY'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('AnimationOffsetX'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('AnimationOffsetY'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('RoofOffsetX'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('RoofOffsetY'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('RoofAnimationOffsetX'));
+        $fields->addFieldToTab('Root.Image/Offsets', NumericField::create('RoofAnimationOffsetY'));
 
         return $fields;
     }
